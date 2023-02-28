fd09f2be52346764347e45531cffb3ddb2503664
Mauroo <100035249+mauroociappinaph@users.noreply.github.com>
2023-02-28 20:05:29 -0300

    Segundo Commit

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -24,8 +24,19 @@ class Presupuesto {
 }
 
 class UI {
-
+insertarPresupuesto (cantidad) {
+const {presupuesto , restante} = cantidad;
+document.querySelector("#total").textContent = presupuesto;
+document.querySelector("#restante").textContent = restante;
 }
+}
+
+//Instancias
+
+const ui = new UI();
+let presupuesto;
+
+
 //Funciones
 
 function preguntarPresupuesto() {
@@ -38,4 +49,11 @@ console.log (Number (presupuestoUsuario));
 
 if (presupuestoUsuario === " " || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0 ) {
     window.location.reload();
+
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
+
+    ui.insertarPresupuesto(presupusto)
 }
+
