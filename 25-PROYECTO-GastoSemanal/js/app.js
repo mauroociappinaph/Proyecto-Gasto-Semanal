08293fc591a7b43b2e58c64f15550a43dab0fd85
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-03-16 12:55:45 -0300

    Cambiar el color del presupuesto restante.
    Los cambios que se realizaron al proyecto son los siguientes:
    
    Se agregó la función comprobarPresupuesto que recibe un objeto con las propiedades presupuesto y restante. Se selecciona el elemento HTML con la clase .restante y se modifica su color de fondo dependiendo del porcentaje de presupuesto restante, siendo rojo si es menor al 25%, amarillo si es menor al 50% y verde si es mayor al 50%.

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -109,6 +109,32 @@ this.limpiarHTML(); //! Elimina el HTML previo
   actualizarRestante(restante){
     document.querySelector("#restante").textContent = restante;
   }
+
+  comprobarPresupuesto(presupuestoObj){
+  const {presupuesto , restante } = presupuestoObj;
+  const restanteDiv = document.querySelector(".restante");
+  //! Comprobar 25% 
+if ((presupuesto / 4) > restante) {
+    restanteDiv.classList.remove ("alert-success" , "alert-warning");
+    restanteDiv.classList.add ("alert-danger");
+
+    
+} else if ((presupuesto /2 ) > restante ){
+  restanteDiv.classList.remove ("alert-success");
+  restanteDiv.classList.add ("alert-warning");
+}
+
+//! Si el total es  0 o menor  
+if (restante <= 0) {
+  ui.imprimirAlerta ("El presupuesto se ha agotado" , "error");
+
+  
+  formulario.querySelector('button[type="submit"]').disabled = true;
+
+}
+
+}
+
 }
 //Instancias
 
@@ -170,6 +196,8 @@ function agregarGasto(e) {
   ui.agregarGastoListado(gastos);
 
   ui.actualizarRestante(restante);
+
+  ui.comprobarPresupuesto(presupuesto);
 
   //! Reiniciar un formulario
   formulario.reset();
